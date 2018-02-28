aec3a12d01080cd31d2b92bbd25c2ff8af2b3980
Mike Samuel <mikesamuel@gmail.com>
2018-02-28 13:55:14 -0500

    added polyfill to index module in example for consistency

--- examples/alice-bob-carol/index.js
+++ examples/alice-bob-carol/index.js
@@ -1,3 +1,10 @@
+'use strict';
+/* Polyfill */
+import {makeFrenemies} from './frenemies.js';
+const frenemies = makeFrenemies('index.js');
+export let {publicKey} = frenemies;
+/* End polyfill */
+
 import {send} from './alice.js';
 
 export let result = send();
